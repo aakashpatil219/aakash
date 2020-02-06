e9c7bd2a5b33e5a9274cb7839b1e98e9ede85220
aakashpatil219 <60725894+aakashpatil219@users.noreply.github.com>
2020-02-06 10:52:28 +0530

    Update checkButton.py

--- checkButton.py
+++ checkButton.py
@@ -1,14 +1,18 @@
 # importing tkinter module
 from tkinter import *
 root = Tk()
+
 # calling var_states when clicked on button
 def var_states():
-    print("Hoppy 1: %d, \nHobby 2: %d, \nHobby 3: %d"%(var1.get(), var2.get(), var3.get()))
+    print("Hoppy 1: %d, \nHobby 2: %d, \nHobby 3: %d" % (var1.get(), var2.get(), var3.get()))
+
 #creating variable like IntVar for getting the value of checkButton (1 or 0)
 var1 = IntVar()
 c1 = Checkbutton(root, text="Hobby 1", variable=var1)
+
 var2 = IntVar()
 c2 = Checkbutton(root, text="Hobby 2", variable=var2)
+
 var3 = IntVar()
 c3 = Checkbutton(root, text="Hobby 3", variable=var3)
 b = Button(root, text="Show", command=var_states)
